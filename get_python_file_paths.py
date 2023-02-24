38203e8b4ae488b0ca8843887a0f070fc34d33d6
leafleaf87 <91792800+leafleaf87@users.noreply.github.com>
2023-02-24 11:53:04 +0500

    Update get_python_file_paths.py
    Fix indentation.

--- get_python_file_paths.py
+++ get_python_file_paths.py
@@ -7,8 +7,8 @@ def main():
         for file in files:
             if file.endswith('.py'):
                 python_file_paths.append(os.path.join(path,file))
-   with open('python_file_paths.txt','w') as f:
-       f.write('\n'.join(python_file_paths))
+    with open('python_file_paths.txt','w') as f:
+        f.write('\n'.join(python_file_paths))
 
 
 if __name__ == '__main__':
