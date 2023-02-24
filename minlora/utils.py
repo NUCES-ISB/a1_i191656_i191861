ef1eda43401799bdbd83b6d64edbd0d06d44ab1a
leafleaf87 <91792800+leafleaf87@users.noreply.github.com>
2023-02-24 14:31:45 +0500

    Update utils.py
    Fix styling issues

--- minlora/utils.py
+++ minlora/utils.py
@@ -3,7 +3,10 @@ from torch import nn
 
 
 def apply_to_lora(fn):
-    """apply a function to LoRAParametrization layers, designed to be used with model.apply"""
+    """
+        apply a function to LoRAParametrization layers
+        designed to be used with model.apply
+    """
 
     def apply_fn(layer):
         if isinstance(layer, LoRAParametrization):
@@ -11,12 +14,16 @@ def apply_to_lora(fn):
 
     return apply_fn
 
+def enable_lora(model):
+    return model.apply(apply_to_lora(lambda x: x.enable_lora()))
 
-enable_lora = lambda model: model.apply(apply_to_lora(lambda x: x.enable_lora()))
-disable_lora = lambda model: model.apply(apply_to_lora(lambda x: x.disable_lora()))
+def disable_lora(model):
+    return model.apply(apply_to_lora(lambda x: x.disable_lora()))
 
 
-# ------------------- helper function for collecting parameters for training/saving -------------------
+#                     ------------------- 
+# helper function for collecting parameters for training/saving 
+#                     -------------------
 
 
 def name_is_lora(name):
@@ -31,7 +38,9 @@ def name_is_bias(name):
     return name.split(".")[-1] == "bias"
 
 
-def get_params_by_name(model, print_shapes=False, name_filter=None):
+def get_params_by_name(model,
+                       print_shapes=False,
+                       name_filter=None):
     for n, p in model.named_parameters():
         if name_filter is None or name_filter(n):
             if print_shapes:
@@ -39,19 +48,27 @@ def get_params_by_name(model, print_shapes=False, name_filter=None):
             yield p
 
 
-def get_lora_params(model, print_shapes=False):
-    return get_params_by_name(model, print_shapes=print_shapes, name_filter=name_is_lora)
+def get_lora_params(model,
+                    print_shapes=False):
+    return get_params_by_name(model,
+                              print_shapes=print_shapes,
+                              name_filter=name_is_lora)
 
 
-def get_bias_params(model, print_shapes=False):
-    return get_params_by_name(model, print_shapes=print_shapes, name_filter=name_is_bias)
+def get_bias_params(model,
+                    print_shapes=False):
+    return get_params_by_name(model,
+                              print_shapes=print_shapes,
+                              name_filter=name_is_bias)
 
 
 def get_lora_state_dict(model):
     return {k: v for k, v in model.state_dict().items() if name_is_lora(k)}
 
 
-# ------------------- helper function for inferencing with multiple lora -------------------
+#              ------------------- 
+# helper function for inferencing with multiple lora
+#              -------------------
 
 
 def _prepare_for_multiple_lora(lora_layer):
@@ -82,19 +99,33 @@ def select_lora(model, index):
     return model
 
 
-# ------------------- helper function for tying and untieing weights -------------------
+#            ------------------- 
+# helper function for tying and untieing weights
+#            -------------------
 
 
 def tie_weights(linear: nn.Linear, embedding: nn.Embedding):
-    """tie the weights of the linear layer and the embedding layer both with the same lora"""
+    """
+        tie the weights of the linear layer and the embedding layer both with the same lora
+    """
     # this line below is optional if the original is already tied
-    embedding.parametrizations.weight.original = linear.parametrizations.weight.original
-    embedding.parametrizations.weight[0].lora_A = linear.parametrizations.weight[0].lora_B
-    embedding.parametrizations.weight[0].lora_B = linear.parametrizations.weight[0].lora_A
+    linear_weight_orig = linear.parametrizations.weight.original
+    linear_lora_B = linear.parametrizations.weight[0].lora_B
+    linear_lora_A = linear.parametrizations.weight[0].lora_A
+    embedding.parametrizations.weight.original = linear_weight_orig
+    embedding.parametrizations.weight[0].lora_A = linear_lora_B
+    embedding.parametrizations.weight[0].lora_B = linear_lora_A
 
 
 def untie_weights(linear: nn.Linear, embedding: nn.Embedding):
-    """untie the weights of the linear layer and the embedding layer"""
-    embedding.parametrizations.weight.original = nn.Parameter(embedding.weight.original.clone())
-    embedding.parametrizations.weight[0].lora_A = nn.Parameter(embedding.parametrizations.weight[0].lora_A.clone())
-    embedding.parametrizations.weight[0].lora_B = nn.Parameter(embedding.parametrizations.weight[0].lora_B.clone())
+    """
+        untie the weights of the linear layer and the embedding layer
+    """
+    embedding_orig = nn.Parameter(embedding.weight.original.clone())
+    lora_A_clone = embedding.parametrizations.weight[0].lora_A.clone()
+    embedding_lora_A = nn.Parameter(lora_A_clone)
+    lora_B_clone = embedding.parametrizations.weight[0].lora_B.clone()
+    embedding_lora_B = nn.Parameter(lora_B_clone)
+    embedding.parametrizations.weight.original = embedding_orig
+    embedding.parametrizations.weight[0].lora_A = embedding_lora_A
+    embedding.parametrizations.weight[0].lora_B = embedding_lora_B
