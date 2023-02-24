2d434077b4dd0f770d92e27d02b21c2c5f61aabc
Asfand Ali Irfan <i191656@nu.edu.pk>
2023-02-24 15:29:56 +0500

    Run Code

--- minlora/model.py
+++ minlora/model.py
@@ -13,24 +13,26 @@ from torch import nn
 
 
 class LoRAParametrization(nn.Module):
-    def __init__(self,
-                 fan_in,
-                 fan_out,
-                 fan_in_fan_out=False,
-                 rank=4,
-                 lora_dropout_p=0.0,
-                 lora_alpha=1):
+    def __init__(
+        self,
+        fan_in,
+        fan_out,
+        fan_in_fan_out=False,
+        rank=4,
+        lora_dropout_p=0.0,
+        lora_alpha=1,
+    ):
         super().__init__()
         # if weight is stored as (fan_out, fan_in),
         #    the memory layout of A & B follows (W + BA)x
         # otherwise,
         #    it's x(W + AB).
-        #This allows us to tie the weights \
+        # This allows us to tie the weights \
         #    between linear layers and embeddings.
         if fan_in_fan_out:
-            self.swap = (lambda x: (x[1], x[0]))
+            self.swap = lambda x: (x[1], x[0])
         else:
-            self.swap = (lambda x: x)
+            self.swap = lambda x: x
         self.lora_A = nn.Parameter(torch.zeros(self.swap((rank, fan_in))))
         self.lora_B = nn.Parameter(torch.zeros(self.swap((fan_out, rank))))
         nn.init.kaiming_uniform_(self.lora_A, a=math.sqrt(5))
@@ -44,26 +46,23 @@ class LoRAParametrization(nn.Module):
             self.dropout_fn = self._dropout
         else:
             lambda x: x
-        self.register_buffer("lora_dropout_mask",
-                             torch.ones(self.swap((1, fan_in)),
-                             dtype=self.lora_A.dtype))
+        self.register_buffer(
+            "lora_dropout_mask",
+            torch.ones(self.swap((1, fan_in)), dtype=self.lora_A.dtype),
+        )
         self.forward_fn = self.lora_forward
 
-    def _dropout(self,
-                 A):
+    def _dropout(self, A):
         # to mimic the original implementation:
         #     A @ dropout(x), we do (A * dropout(ones)) @ x
         return A * self.lora_dropout(self.lora_dropout_mask)
 
-    def lora_forward(self,
-                     X):
-        swap_input = (self.lora_B,
-                      self.dropout_fn(self.lora_A))
+    def lora_forward(self, X):
+        swap_input = (self.lora_B, self.dropout_fn(self.lora_A))
         mat = torch.mm(*self.swap(swap_input)).view(X.shape)
-        return X +  mat * self.scaling
+        return X + mat * self.scaling
 
-    def forward(self,
-                X):
+    def forward(self, X):
         return self.forward_fn(X)
 
     def disable_lora(self):
@@ -73,11 +72,7 @@ class LoRAParametrization(nn.Module):
         self.forward_fn = self.lora_forward
 
     @classmethod
-    def from_linear(cls,
-                    layer,
-                    rank=4,
-                    lora_dropout_p=0.0,
-                    lora_alpha=1):
+    def from_linear(cls, layer, rank=4, lora_dropout_p=0.0, lora_alpha=1):
         fan_out, fan_in = layer.weight.shape
         return cls(
             fan_in,
@@ -85,15 +80,11 @@ class LoRAParametrization(nn.Module):
             fan_in_fan_out=False,
             rank=rank,
             lora_dropout_p=lora_dropout_p,
-            lora_alpha=lora_alpha
+            lora_alpha=lora_alpha,
         )
 
     @classmethod
-    def from_conv2d(cls,
-                    layer,
-                    rank=4,
-                    lora_dropout_p=0.0,
-                    lora_alpha=1):
+    def from_conv2d(cls, layer, rank=4, lora_dropout_p=0.0, lora_alpha=1):
         weight_view = layer.weight.view(layer.weight.shape[0], -1)
         fan_out, fan_in = weight_view.shape
         return cls(
@@ -102,15 +93,11 @@ class LoRAParametrization(nn.Module):
             fan_in_fan_out=False,
             rank=rank,
             lora_dropout_p=lora_dropout_p,
-            lora_alpha=lora_alpha
+            lora_alpha=lora_alpha,
         )
 
     @classmethod
-    def from_embedding(cls,
-                       layer,
-                       rank=4,
-                       lora_dropout_p=0.0,
-                       lora_alpha=1):
+    def from_embedding(cls, layer, rank=4, lora_dropout_p=0.0, lora_alpha=1):
         fan_in, fan_out = layer.weight.shape
         return cls(
             fan_in,
@@ -118,24 +105,18 @@ class LoRAParametrization(nn.Module):
             fan_in_fan_out=True,
             rank=rank,
             lora_dropout_p=lora_dropout_p,
-            lora_alpha=lora_alpha
+            lora_alpha=lora_alpha,
         )
 
-   
+
 # specify which layers to add lora to
 # by default only add to linear layers
 default_lora_config = {
-    nn.Linear: {
-        "weight": partial(LoRAParametrization.from_linear,
-                          rank=4),
-    },
+    nn.Linear: {"weight": partial(LoRAParametrization.from_linear, rank=4),},
 }
 
 
-def apply_lora(layer,
-               register=True,
-               merge=False,
-               lora_config=default_lora_config):
+def apply_lora(layer, register=True, merge=False, lora_config=default_lora_config):
     """
         add lora parametrization to a layer
         designed to be used with model.apply
@@ -145,19 +126,18 @@ def apply_lora(layer,
         if layer_type in lora_config:
             layer_type_config = lora_config[layer_type].items()
             for attr_name, parametrization in layer_type_config:
-                parametrize.register_parametrization(layer,
-                                                     attr_name,
-                                                     parametrization(layer))
+                parametrize.register_parametrization(
+                    layer, attr_name, parametrization(layer)
+                )
     else:  # this will remove all parametrizations, use with caution
         if hasattr(layer, "parametrizations"):
             for attr_name in layer.parametrizations.keys():
-                parametrize.remove_parametrizations(layer,
-                                                    attr_name,
-                                                    leave_parametrized=merge)
+                parametrize.remove_parametrizations(
+                    layer, attr_name, leave_parametrized=merge
+                )
 
 
-def add_lora(model,
-             lora_config=default_lora_config):
+def add_lora(model, lora_config=default_lora_config):
     """
         add lora parametrization to all layers in a model.
         Calling it twice will add lora twice
