b5e94c702e3355cffda9ab9c94c5d890e6034754
leafleaf87 <91792800+leafleaf87@users.noreply.github.com>
2023-02-24 11:55:21 +0500

    Update get_python_file_paths.py
    Add whitespaces after commas.

--- get_python_file_paths.py
+++ get_python_file_paths.py
@@ -6,8 +6,8 @@ def main():
     for path, subdirs, files in os.walk('.'):
         for file in files:
             if file.endswith('.py'):
-                python_file_paths.append(os.path.join(path,file))
-    with open('python_file_paths.txt','w') as f:
+                python_file_paths.append(os.path.join(path, file))
+    with open('python_file_paths.txt', 'w') as f:
         f.write('\n'.join(python_file_paths))
 
 
