fe5854f25c81a42f52107e952fc886dd07fa1339
leafleaf87 <91792800+leafleaf87@users.noreply.github.com>
2023-02-24 20:04:16 +0500

    Update model.py

--- minlora/model.py
+++ minlora/model.py
@@ -112,7 +112,7 @@ class LoRAParametrization(nn.Module):
 # specify which layers to add lora to
 # by default only add to linear layers
 default_lora_config = {
-    nn.Linear: {"weight": partial(LoRAParametrization.from_linear, rank=4),},
+    nn.Linear: {"weight": partial(LoRAParametrization.from_linear, rank=4),}
 }
 
 
@@ -147,7 +147,7 @@ def add_lora(model, lora_config=default_lora_config):
 
 def merge_lora(model):
     """
-        merge lora parametrization to all layers in a model. 
+        merge lora parametrization to all layers in a model.
         This will remove all parametrization
     """
     model.apply(partial(apply_lora, register=False, merge=True))
