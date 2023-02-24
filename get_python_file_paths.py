c8ee949c58bb27217e9bb664fd52c8bb412e8ec5
Asfand Ali Irfan <i191656@nu.edu.pk>
2023-02-24 11:35:02 +0500

    Add workflow

---
+++ get_python_file_paths.py
@@ -0,0 +1,13 @@
+import os
+
+def main():
+	python_file_paths = []
+	for path,subdirs,files in os.walk('.'):
+		for file in files:
+			if file.endswith('.py'):
+				python_file_paths.append(os.path.join(path,file))
+	with open('python_file_paths.txt','w') as f:
+		f.write('\n'.join(python_file_paths))	
+
+if __name__ == '__main__':
+	main()
